a541059290f3ddd7d850d4d099f2ece899d71d55
juanmata42 <81301176+juanmata42@users.noreply.github.com>
2021-12-13 17:02:53 +0100

    Update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://s7anm68krjw.p51.rt3.io/' });
+const API = axios.create({ baseURL: 'https://z4gsx72mdnf.p55.rt3.io' });
 
 /* this might not be needed
 API.interceptors.request.use((req:) => {
