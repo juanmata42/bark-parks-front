6d597aa68b7148d12d24216175f6f2dbec1a5457
juanmata42 <81301176+juanmata42@users.noreply.github.com>
2021-12-13 20:58:38 +0100

    Another remoteit change

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://z4gsx72mdnf.p55.rt3.io' });
+const API = axios.create({ baseURL: 'https://x3p8cjhi7o5.p50.rt3.io' });
 
 /* this might not be needed
 API.interceptors.request.use((req:) => {
